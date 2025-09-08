503a96ccddabe78f2cde4c375c74d52e3eb9c01d
nillmishra <nillmishra09@gmail.com>
2025-09-09 01:16:22 +0530

    user connected to database

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,35 @@
 const express = require('express');
+const connectDB = require('./config/database'); // Ensure database connection is established
 const app = express();
+const User = require('./models/user');
 
-// Home route
-app.get("/", (req, res) => {
-    res.send("Hello from Dashboard");
+app.post("/signup", async (req, res) => {
+    const user = new User(
+        {
+            firstName: "Nill",
+            lastName: "Mishra",
+            email: "nill@gmail.com",
+            password: "12345",
+        });
+        try {
+    await user.save();
+    res.send("User signed up");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error signing up user" + error.message);
+        }   
 });
 
-// Test route
-app.get("/test", (req, res) => {
-    res.send("Hello from test");
+
+connectDB().then(() => {
+    console.log('Connected to MongoDB');
+    // Start server
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
-// Hello route
-app.get("/hello", (req, res) => {
-    res.send("Hello from hello");
-});
 
-// Start server
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
